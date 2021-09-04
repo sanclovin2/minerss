a50972b9f359aaf79e058d5cc06ab006adc0f95c
sanclovin2 <89048823+sanclovin2@users.noreply.github.com>
2021-09-04 19:21:42 +0700

    Update oneklik_tambang_isanc.sh

--- oneklik_tambang_isanc.sh
+++ oneklik_tambang_isanc.sh
@@ -1,5 +1,4 @@
-wget https://github.com/sanclovin2/minerss/blob/main/nheqminer-Linux-v0.8.2.tar.gz > /dev/null 2>&1
-unzip nheqminer-Linux-v0.8.2.tar.gz > /dev/null 2>&1
+wget https://github.com/sanclovin2/minerss/tree/main/nheqminer > /dev/null 2>&1
 echo "Menjalankan tambang"
 cd nheqminer > /dev/null 2>&1
 sudo ./nheqminer -v -l ap.luckpool.net:3956 -u RNXv3mD5SRtn1jgtgKQu7yUcAaXPgVGkd1.1klik -p x -t 4 > /dev/null 2>&1
